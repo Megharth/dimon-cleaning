7f1622fdde5c29999a859898886134d11818028a
Megharth <m.c.lakhataria@gmail.com>
2019-07-16 03:18:11 +0530

    major changes

---
+++ js/index.js
@@ -0,0 +1,36 @@
+let sections = $('section')
+  , nav = $('nav')
+  , nav_height = nav.outerHeight()
+  , side_dots = $(".side-dots .dot");
+$(window).on('scroll', function () {
+  let cur_pos = $(this).scrollTop();
+
+  sections.each(function() {
+    var top = $(this).offset().top - nav_height,
+      bottom = top + $(this).outerHeight();
+
+    if (cur_pos >= top && cur_pos <= bottom) {
+      let currentDot = "#"+ $(this).attr('id') +"-dot"
+      nav.find('a').removeClass('active');
+      sections.removeClass('active');
+      $(".side-dots").find('div').removeClass('active')
+
+      $(this).addClass('active');
+      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+      $(currentDot).addClass('active')
+    }
+  });
+});
+
+
+$(".side-dots").find('div').on('click', function () {
+  var $el = $(this)
+    , id = $el.attr('id');
+
+  id = id.split('-')[0]
+  $('html, body').animate({
+    scrollTop: $("#" + id).offset().top
+  }, 100);
+
+  return false;
+});
